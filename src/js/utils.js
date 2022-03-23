f05e29e46b51c4e461d02a6317d07277713a9fc9
Mary Lou <crnacura@gmail.com>
2022-03-23 06:07:21 +0000

    Update utils.js
    Fixed mouse position when scrolling the page

--- src/js/utils.js
+++ src/js/utils.js
@@ -7,20 +7,11 @@ const lerp = (a, b, n) => (1 - n) * a + n * b;
 const clamp = (num, min, max) => num <= min ? min : num >= max ? max : num;
 
 // Gets the mouse position
-const getMousePos = (e) => {
-    let posx = 0;
-    let posy = 0;
-    if (!e) e = window.event;
-    if (e.pageX || e.pageY) {
-        posx = e.pageX;
-        posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY)    {
-        posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
-        posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
-    }
-    
-    return { x : posx, y : posy }
+const getMousePos = (ev) => {
+    return { 
+        x : ev.clientX, 
+        y : ev.clientY 
+    };
 };
 
 export { map, lerp, clamp, getMousePos };
